2819493b5b62a50da2d54d2f362a76e2d0c002d7
HAISE SASAKI <154090375+HAISESASAK-I@users.noreply.github.com>
2026-07-19 00:01:47 +0500

    Create the function t performCalculation

--- calculator.js
+++ calculator.js
@@ -1,7 +1,7 @@
 const numbersContainer = document.querySelector(".numbers");
 const operatorsContainer = document.querySelector(".operators");
-const belowDisplay = document.querySelector(".below-display");
-const aboveDisplay = document.querySelector(".above-display");
+const calculatorBelowDisplay = document.querySelector(".below-display");
+const calculatorAboveDisplay = document.querySelector(".above-display");
 numbersContainer.addEventListener("click", (event) => {
     //user can click on the gap between the buttons and that can cause the click event to get
     //activated on container. SO, this following validation explicitly let only numbers to pass 
@@ -10,25 +10,33 @@ numbersContainer.addEventListener("click", (event) => {
     if (!event.target.matches(".number")) {
         return;
     }
-    updateCalculatorDisplayWithDigit(event, belowDisplay);
+    updateCalculatorDisplayWithDigit(event, calculatorBelowDisplay);
 });
-function updateCalculatorDisplayWithDigit(event, belowDisplay) {
-    belowDisplay.textContent = belowDisplay.textContent + event.target.textContent;
+function updateCalculatorDisplayWithDigit(event, calculatorBelowDisplay) {
+    calculatorBelowDisplay.textContent = calculatorBelowDisplay.textContent + event.target.textContent;
 }
 operatorsContainer.addEventListener("click", (event) => {
     if (!event.target.matches(".operator")) {
         return;
     }
-    updateCalculatorDisplayWithOperator(event, aboveDisplay, belowDisplay);
-})
-function updateCalculatorDisplayWithOperator(event, aboveDisplay, belowDisplay) {
-
+    if (event.target.matches(".equal")) {
+        performCalculation(calculatorBelowDisplay, calculatorAboveDisplay);
+    }
+    updateCalculatorDisplayWithOperator(event, calculatorAboveDisplay, calculatorBelowDisplay);
+});
+function updateCalculatorDisplayWithOperator(event, calculatorAboveDisplay, calculatorBelowDisplay) {
     //This transfer of text content form below display of calculator to above will help in checking things like 
     //if there is only one operator or one point is used with a number
     //The use of space is because it will help in extracting multi digits using split method like 1000 + 
-    aboveDisplay.textContent = `${belowDisplay.textContent} ${event.target.textContent}`;
-    clearDisplay(belowDisplay);
+    calculatorAboveDisplay.textContent = `${calculatorBelowDisplay.textContent} ${event.target.textContent}`;
+    clearDisplay(calculatorBelowDisplay);
 
+}
+function performCalculation(calculatorBelowDisplay, calculatorAboveDisplay) {
+    let numberAndOperator = calculatorAboveDisplay.textContent.split(" ");
+    let firstNumber = +numberAndOperator[0];
+    let operator = numberAndOperator[1];
+    let secondNumber = +calculatorBelowDisplay.textContent;
 }
 function clearDisplay(display) {
     display.textContent = "";
