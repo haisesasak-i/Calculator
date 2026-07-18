306fb1049af7876a6378d273d37e193c1958a3b7
HAISE SASAKI <154090375+HAISESASAK-I@users.noreply.github.com>
2026-07-18 16:57:51 +0500

    Create the basic calculator opeations functions in calculator.js

---
+++ calculator.js
@@ -0,0 +1,12 @@
+function add(firstNumber = 0, secondNumber = 0) {
+    return firstNumber + secondNumber;
+}
+function subtract(firstNumber = 0, secondNumber = 0) {
+    return firstNumber - secondNumber;
+}
+function multiply(firstNumber = 0, secondNumber = 0) {
+    return firstNumber * secondNumber;
+}
+function divide(firstNumber = 0, secondNumber = 1) {
+    return firstNumber / secondNumber;
+}
